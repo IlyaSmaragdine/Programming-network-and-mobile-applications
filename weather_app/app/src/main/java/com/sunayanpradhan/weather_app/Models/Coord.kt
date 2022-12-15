2aab1f76fa9e9dcbef3cac41cf794048fa745da3
Elias <116229203+IlyaSmaragdine@users.noreply.github.com>
2022-12-15 23:29:12 +0200

    Delete weather_app directory

--- weather_app/app/src/main/java/com/sunayanpradhan/weather_app/Models/Coord.kt
+++
@@ -1,6 +0,0 @@
-package com.example.weather_app.Models
-
-data class Coord(
-    val lat: Double,
-    val lon: Double
-)
--- weather_app/app/src/main/java/com/sunayanpradhan/weather_app/Models/Rain.kt
+++
@@ -1,5 +0,0 @@
-package com.example.weather_app.Models
-
-data class Rain(
-    val `1h`: Double
-)
