691e06438a92e1a959a4ac0d9255866cefb81aca
Elias <6960628@stud.nau.edu.ua>
2022-12-15 23:31:44 +0200

    ?

--- WeatherApp/app/src/main/java/com/example/weather_app/Models/Clouds.kt
+++
@@ -1,5 +0,0 @@
-package com.example.weather_app.Models
-
-data class Clouds(
-    val all: Int
-)
--- WeatherApp/app/src/main/java/com/example/weather_app/Models/Sys.kt
+++
@@ -1,9 +0,0 @@
-package com.example.weather_app.Models
-
-data class Sys(
-    val country: String,
-    val id: Int,
-    val sunrise: Int,
-    val sunset: Int,
-    val type: Int
-)
