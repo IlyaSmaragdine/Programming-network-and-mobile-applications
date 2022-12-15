d7294ea1eb44e33bdd5135d4007206127a42995d
Elias <116229203+IlyaSmaragdine@users.noreply.github.com>
2022-12-15 22:30:53 +0200

    add a new proj
    add a new proj

---
+++ WeatherApp/app/src/main/java/com/example/weather_app/Models/Clouds.kt
@@ -0,0 +1,5 @@
+package com.example.weather_app.Models
+
+data class Clouds(
+    val all: Int
+)
---
+++ weather_app/app/src/main/java/com/sunayanpradhan/weather_app/Models/Wind.kt
@@ -0,0 +1,7 @@
+package com.example.weather_app.Models
+
+data class Wind(
+    val deg: Int,
+    val gust: Double,
+    val speed: Double
+)
